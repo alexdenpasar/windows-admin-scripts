b68b0d13c1415626e25135e97026a438c078de84
Alexander Kovrovskiy <a.s.kovrovskiy@gmail.com>
2025-11-21 16:29:34 +0300

    Sending an email with a script

---
+++ Email_bad_d.ps1
@@ -0,0 +1,8 @@
+﻿$DateTime = Get-Date -Format “dd-MM-yyyy HH:mm:ss”
+$EmailFrom = “1c@server.com”
+$EmailTo = “user@server.com”
+$Subject = “[ERROR] Backup database 1c”
+$Body = “Error of daily backup of 1C database. Show logs on srv-lib. Completion time: $DateTime”
+$SmtpServer = “smtp.server.com”
+$smtp = New-Object net.mail.smtpclient($SmtpServer)
+$smtp.Send($EmailFrom, $EmailTo, $Subject, $Body)
