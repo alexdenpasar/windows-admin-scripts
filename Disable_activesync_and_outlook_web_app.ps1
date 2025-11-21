ab333ee8ada6bbd7bc38f0058d18f558a6cb6475
Alexander Kovrovskiy <a.s.kovrovskiy@gmail.com>
2025-11-21 16:29:24 +0300

    Added the Disable Activesync script to Exchange.

---
+++ Disable_activesync_and_outlook_web_app.ps1
@@ -0,0 +1,10 @@
+﻿$ServerFQDN = "exchangeserver"
+$mailbox =  $args[0]
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ServerFQDN/PowerShell/ -Authentication Kerberos
+Import-PSSession $Session
+
+
+Set-CASMailbox -Identity $mailbox -ActiveSyncEnabled $False
+Set-CASMailbox -Identity $mailbox -OWAEnabled $False
+
+Remove-PSSession $Session
