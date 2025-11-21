3b1bc847e3716127d41c72a45ec94a6f8c06f199
Alexander Kovrovskiy <a.s.kovrovskiy@gmail.com>
2025-11-21 16:29:40 +0300

    Sending an email with a script

---
+++ Email_Good_m.ps1
@@ -0,0 +1,8 @@
+﻿$DateTime = Get-Date -Format “dd-MM-yyyy HH:mm:ss”
+$EmailFrom = “1c@server.com”
+$EmailTo = “user@server.com”
+$Subject = “[Success] Backup database 1c”
+$Body = “Backup of 1C databases has been successfully completed. Completion time: $DateTime”
+$SmtpServer = “smtp.server.com”
+$smtp = New-Object net.mail.smtpclient($SmtpServer)
+$smtp.Send($EmailFrom, $EmailTo, $Subject, $Body)
