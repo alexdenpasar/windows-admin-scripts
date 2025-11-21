3a94c5594cc70f9a9babbd737050ae4027cfacab
Alexander Kovrovskiy <a.s.kovrovskiy@gmail.com>
2025-11-21 16:25:44 +0300

    123

--- Get-LargeFiles.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem C:\ -Force -File -ErrorAction SilentlyContinue | 
-    Where-Object {$_.Length -gt 1GB} | 
-    Sort-Object Length -Descending | 
-    Select-Object FullName, @{Name="Size(GB)";Expression={[math]::Round($_.Length/1GB,2)}} | 
-    Format-Table -AutoSize
